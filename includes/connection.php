156ec82aefcdead4ce97b66adcf702740a9d1f99
ngomile <ngomile932@gmail.com>
2022-09-06 11:08:48 +0200

    Implement logic to initiate database

--- includes/connection.php
+++ includes/connection.php
@@ -26,23 +26,27 @@ class DBConnection
      */
     public static function init()
     {
-        // $schema = generateSQL();
+        $schema = self::generateSQL();
 
-        // try {
-        //     $dbh = new PDO("mysql:host=localhost", "root", "");
-        //     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //     $dbQuery = $dbh->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'farmersconnect'");
-        //     // Create the farmersconnect database if it does not exist
-        //     if (!$dbQuery->fetch()) {
-        //         $dbh->beginTransaction();
-        //         $dbh->query("CREATE DATABASE IF NOT EXISTS farmersconnect");
-        //         $dbh->query("USE farmersconnect");
+        try {
+            $dbh = new PDO("mysql:host=localhost", "root", "");
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbQuery = $dbh->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'vms'");
+            // Create the vms database if it does not exist
+            if (!$dbQuery->fetch()) {
+                $dbh->beginTransaction();
+                $dbh->query("CREATE DATABASE IF NOT EXISTS vms");
+                $dbh->query("USE vms");
 
-        //         $dbh->query($schema);
-        //         $dbh->commit();
-        //     }
-        // } catch (PDOException $e) {
-        //     die("Database connection failed: " . $e->getMessage());
-        // }
+                $dbh->query($schema);
+                $dbh->commit();
+            }
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    private static function generateSQL()
+    {
     }
 }

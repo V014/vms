796983aac1348a82d5a11f20bb361422d5028a88
ngomile <ngomile932@gmail.com>
2022-09-11 20:59:02 +0200

    Implement method to retrieve all orders

--- includes/entity/order.php
+++ includes/entity/order.php
@@ -2,11 +2,29 @@
 
 class Order
 {
-    const TABLE = "";
-    const COLUMNS = "";
+    const TABLE = "orders";
+    const COLUMNS = "c.name, SET_X(c.location) AS longitude, SET_Y(c.location) AS latitude, c.established, f.name, o.quantity, o.cost, o.status, o.order_date";
 
+    /*
+    Returns all the orders made by companies so far
+     */
     public static function all()
     {
+        $orders = [];
+        $connection = DBConnection::getConnection();
+        $table = self::TABLE;
+        $columns = self::COLUMNS;
+
+        $query = "SELECT {$columns} FROM {$table} INNER JOIN companies AS c ON c.id = o.company_id INNER JOIN fuel_types AS f ON f.id = f.type_id";
+        $sth = $connection->prepare($query);
+
+        if ($sth->execute()) {
+            foreach ($sth->fetchAll() as $order) {
+                $orders[] = new Order();
+            }
+        }
+
+        return $orders;
     }
 
     public static function byCompany($id)

f90fd585388084fcc6d9d9ec57425367b9cf3b36
ngomile <ngomile932@gmail.com>
2022-09-14 18:51:30 +0200

    Initial implementation of logic to redirect user from index page when logged in based on role

--- index.php
+++ index.php
@@ -8,3 +8,17 @@ DBConnection::init();
 if (!Auth::isLoggedIn()) {
     redirect(BASE_DIR . "home.php");
 }
+
+$user = Auth::getUser();
+
+switch ($user->role) {
+    case 'admin':
+        # code...
+        break;
+    case 'driver':
+        # code...
+        break;
+    case 'company':
+        # code...
+        break;
+}

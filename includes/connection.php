98ae7bd59458f44e9d25e42efdc7acf6113fc742
ngomile <ngomile932@gmail.com>
2022-09-15 15:37:55 +0200

    Add helper to check if database has been configured

--- includes/connection.php
+++ includes/connection.php
@@ -15,6 +15,10 @@ class DBConnection
                 self::$dbh = new PDO("mysql:host=localhost;dbname=vms", "root", "");
                 self::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+                if (!self::isSetUp(self::$dbh)) {
+                    self::init();
+                }
             }
 
             return self::$dbh;
@@ -46,6 +50,17 @@ class DBConnection
         } catch (PDOException $e) {
             die("Database connection failed: " . $e->getMessage());
         }
+    }
+
+    public static function isSetUp($dbh)
+    {
+        $dbQuery = $dbh->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'vms'");
+        // Create the vms database if it does not exist
+        if (!$dbQuery->fetch()) {
+            return false;
+        }
+
+        return true;
     }
 
     private static function generateSQL()

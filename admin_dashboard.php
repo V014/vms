9fd7cddf4426f79fd8d0b22a209333f14b177770
ngomile <ngomile932@gmail.com>
2022-09-16 16:12:36 +0200

    Fix footer placement

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -272,12 +272,12 @@ $user = Auth::getUser();
                     </div>
                 </div>
             </div>
+            <footer class="bg-white sticky-footer">
+                <div class="container my-auto">
+                    <div class="text-center my-auto copyright"><span>Copyright © VMS 2022</span></div>
+                </div>
+            </footer>
         </div>
-        <footer class="bg-white sticky-footer">
-            <div class="container my-auto">
-                <div class="text-center my-auto copyright"><span>Copyright © Brand 2022</span></div>
-            </div>
-        </footer>
     </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>

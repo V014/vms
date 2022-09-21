1889909bf9c3a1e0d9bdbff3a4c3ab79b6200884
ngomile <ngomile932@gmail.com>
2022-09-21 10:09:02 +0200

    company entity fix incorrectly created sql

--- includes/entity/company.php
+++ includes/entity/company.php
@@ -6,7 +6,7 @@ include_once dirname(__FILE__) . "/../utils.php";
 class Company
 {
     const TABLE = "companies";
-    const COLUMNS = "u.id AS user_id, u.created_at, c.name, c.established, ST_X(c.location) AS longitude, ST_Y(c.location) AS latitude, COUNT(o.id) AS total_orders, SUM(o.cost) AS total_profit";
+    const COLUMNS = "u.id AS user_id, u.created_at, c.name, c.established, ST_X(c.location) AS longitude, ST_Y(c.location) AS latitude, COUNT(o.id) AS total_orders, SUM(o.cost) AS total_spend";
 
     public $userID;
     public $name;
@@ -14,7 +14,7 @@ class Company
     public $latitude;
     public $longitude;
     public $totalOrders;
-    public $totalProfit;
+    public $totalSpend;
     public $createdAt;
 
     public function __construct($data)
@@ -25,7 +25,7 @@ class Company
         $this->latitude = $data["latitude"];
         $this->longitude = $data["longitude"];
         $this->totalOrders = $data["total_orders"];
-        $this->totalProfit = number_format($data["total_profit"]);
+        $this->totalSpend = number_format($data["total_spend"]);
         $this->createdAt = $data["created_at"];
     }
 
@@ -51,7 +51,7 @@ class Company
         $connection = DBConnection::getConnection();
         $table = self::TABLE;
         $columns = self::COLUMNS;
-        $query = "SELECT {$columns} FROM {$table} INNER JOIN users AS u ON u.id = c.user_id";
+        $query = "SELECT {$columns} FROM {$table} AS c INNER JOIN users AS u ON u.id = c.user_id INNER JOIN orders AS o ON o.company_id = c.id WHERE c.id = :id";
         $sth = $connection->prepare($query);
 
         if (!$sth->execute([":id" => $id])) {

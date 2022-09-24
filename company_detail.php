9edacd0fa591540fee7cee3d888651327b172885
ngomile <ngomile932@gmail.com>
2022-09-24 12:09:39 +0200

    company_detail Display company information

--- company_detail.php
+++ company_detail.php
@@ -1,10 +1,10 @@
 <?php
 include_once "./includes/utils.php";
 include_once "./includes/entity/company.php";
-include_once "./includes/entity/USER.php";
+include_once "./includes/entity/user.php";
 
 $company = Company::find($_GET["id"]);
-$user = User::find($company->userID);
+$userCompany = User::find($company->userID);
 
 ?>
 
@@ -37,6 +37,7 @@ $user = User::find($company->userID);
                     <!-- Profile Section -->
                     <section style="background-color: #eee;">
                         <div class="container py-5">
+                            <!-- Breadcrumb Section -->
                             <div class="row">
                                 <div class="col">
                                     <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
@@ -66,40 +67,9 @@ $user = User::find($company->userID);
                                 <div class="col-lg-4">
                                     <div class="card mb-4">
                                         <div class="card-body text-center">
-                                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-                                            <h5 class="my-3">John Smith</h5>
-                                            <p class="text-muted mb-1">Full Stack Developer</p>
-                                            <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
-                                            <div class="d-flex justify-content-center mb-2">
-                                                <button type="button" class="btn btn-primary">Follow</button>
-                                                <button type="button" class="btn btn-outline-primary ms-1">Message</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card mb-4 mb-lg-0">
-                                        <div class="card-body p-0">
-                                            <ul class="list-group list-group-flush rounded-3">
-                                                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                                    <i class="fas fa-globe fa-lg text-warning"></i>
-                                                    <p class="mb-0">https://mdbootstrap.com</p>
-                                                </li>
-                                                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                                    <i class="fab fa-github fa-lg" style="color: #333333;"></i>
-                                                    <p class="mb-0">mdbootstrap</p>
-                                                </li>
-                                                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                                    <i class="fab fa-twitter fa-lg" style="color: #55acee;"></i>
-                                                    <p class="mb-0">@mdbootstrap</p>
-                                                </li>
-                                                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                                    <i class="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
-                                                    <p class="mb-0">mdbootstrap</p>
-                                                </li>
-                                                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                                    <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
-                                                    <p class="mb-0">mdbootstrap</p>
-                                                </li>
-                                            </ul>
+                                            <img src="<?php echo $userCompany->profilePicture; ?>" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+                                            <h5 class="my-3"><?php echo $company->name; ?></h5>
+                                            <p class="text-muted mb-1"><?php echo $company->established; ?></p>
                                         </div>
                                     </div>
                                 </div>
@@ -108,10 +78,10 @@ $user = User::find($company->userID);
                                         <div class="card-body">
                                             <div class="row">
                                                 <div class="col-sm-3">
-                                                    <p class="mb-0">Full Name</p>
+                                                    <p class="mb-0">Company Name</p>
                                                 </div>
                                                 <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">Johnatan Smith</p>
+                                                    <p class="text-muted mb-0"><?php echo $company->name; ?></p>
                                                 </div>
                                             </div>
                                             <hr>
@@ -120,7 +90,7 @@ $user = User::find($company->userID);
                                                     <p class="mb-0">Email</p>
                                                 </div>
                                                 <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">example@example.com</p>
+                                                    <p class="text-muted mb-0"><?php echo $userCompany->email; ?></p>
                                                 </div>
                                             </div>
                                             <hr>
@@ -129,62 +99,16 @@ $user = User::find($company->userID);
                                                     <p class="mb-0">Phone</p>
                                                 </div>
                                                 <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">(097) 234-5678</p>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Mobile</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">(098) 765-4321</p>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Address</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                                                    <p class="text-muted mb-0"><?php echo $userCompany->phoneNumber; ?></p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-md-6">
+                                        <div class="col-md-12">
                                             <div class="card mb-4 mb-md-0">
                                                 <div class="card-body">
-                                                    <p class="mb-4"><span class="text-primary font-italic me-1">assigment</span> Project Status
-                                                    </p>
-                                                    <p class="mb-1" style="font-size: .77rem;">Web Design</p>
-                                                    <div class="progress rounded" style="height: 5px;">
-                                                        <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                                    </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                                                    <div class="progress rounded" style="height: 5px;">
-                                                        <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                                                    </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                                                    <div class="progress rounded" style="height: 5px;">
-                                                        <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                                                    </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                                                    <div class="progress rounded" style="height: 5px;">
-                                                        <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                                                    </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                                                    <div class="progress rounded mb-2" style="height: 5px;">
-                                                        <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="card mb-4 mb-md-0">
-                                                <div class="card-body">
-                                                    <p class="mb-4"><span class="text-primary font-italic me-1">assigment</span> Project Status
+                                                    <p class="mb-4"><span class="text-primary font-italic me-1"><a href="orders_list.php">orders</a></span> Order Status and Details
                                                     </p>
                                                     <p class="mb-1" style="font-size: .77rem;">Web Design</p>
                                                     <div class="progress rounded" style="height: 5px;">

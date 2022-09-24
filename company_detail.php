ac225d12caacbb043074a5a344b039e586095466
ngomile <ngomile932@gmail.com>
2022-09-24 12:24:37 +0200

    company_detail Change titles of informative paragraphs

--- company_detail.php
+++ company_detail.php
@@ -110,25 +110,29 @@ $userCompany = User::find($company->userID);
                                                 <div class="card-body">
                                                     <p class="mb-4"><span class="text-primary font-italic me-1"><a href="orders_list.php">orders</a></span> Order Status and Details
                                                     </p>
-                                                    <p class="mb-1" style="font-size: .77rem;">Web Design</p>
+                                                    <p class="mb-1" style="font-size: .77rem;">Total Orders</p>
                                                     <div class="progress rounded" style="height: 5px;">
                                                         <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                                                     </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
+                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Total Spend</p>
                                                     <div class="progress rounded" style="height: 5px;">
                                                         <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
                                                     </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
+                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Diesel Orders</p>
                                                     <div class="progress rounded" style="height: 5px;">
                                                         <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
                                                     </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
+                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Petrol Orders</p>
                                                     <div class="progress rounded" style="height: 5px;">
                                                         <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                                                     </div>
-                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                                                    <div class="progress rounded mb-2" style="height: 5px;">
-                                                        <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Deliveries Fulfilled</p>
+                                                    <div class="progress rounded" style="height: 5px;">
+                                                        <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                    <p class="mt-4 mb-1" style="font-size: .77rem;">Deliveries Pending</p>
+                                                    <div class="progress rounded" style="height: 5px;">
+                                                        <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                                                     </div>
                                                 </div>
                                             </div>

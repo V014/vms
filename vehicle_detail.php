cd474759b39a243f5d74ef8e3956c2d9f0e66578
ngomile <ngomile932@gmail.com>
2022-09-29 20:47:26 +0200

    vehicle_detail display extra information

--- vehicle_detail.php
+++ vehicle_detail.php
@@ -1,5 +1,8 @@
 <?php
 include_once "./includes/utils.php";
+include_once "./includes/entity/vehicle.php";
+
+$vehicle = Vehicle::find($_GET["id"]);
 
 ?>
 
@@ -29,7 +32,133 @@ include_once "./includes/utils.php";
                     </div>
                 </nav>
                 <div class="container-fluid">
-                    <h3 class="text-dark mb-1">Blank Page</h3>
+                    <!-- Vehicle Section -->
+                    <section style="background-color: #eee;">
+                        <div class="container py-5">
+                            <!-- Breadcrumb Section -->
+                            <div class="row">
+                                <div class="col">
+                                    <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+                                        <?php
+                                        if ($authUser->role === "admin") {
+                                        ?>
+                                            <ol class="breadcrumb mb-0">
+                                                <li class="breadcrumb-item"><a href="<?php echo BASE_DIR . "admin_dashboard.php"; ?>">Home</a></li>
+                                                <li class="breadcrumb-item"><a href="<?php echo BASE_DIR . "orders_list.php"; ?>">Orders</a></li>
+                                                <li class="breadcrumb-item active" aria-current="page"><?php echo $company->name; ?></li>
+                                            </ol>
+                                        <?php
+                                        } elseif ($authUser->role === "company") {
+                                        ?>
+                                            <ol class="breadcrumb mb-0">
+                                                <li class="breadcrumb-item"><a href="<?php echo BASE_DIR . "company_dashboard.php"; ?>">Home</a></li>
+                                                <li class="breadcrumb-item"><a href="<?php echo BASE_DIR . "orders_list.php"; ?>">Orders</a></li>
+                                                <li class="breadcrumb-item active" aria-current="page"><?php echo $company->name; ?></li>
+                                            </ol>
+                                        <?php
+                                        } else {
+                                        ?>
+                                            <ol class="breadcrumb mb-0">
+                                                <li class="breadcrumb-item"><a href="<?php echo BASE_DIR . "driver_dashboard.php"; ?>">Home</a></li>
+                                                <li class="breadcrumb-item"><a href="<?php echo BASE_DIR . "orders_list.php"; ?>">Order</a></li>
+                                                <li class="breadcrumb-item active" aria-current="page"><?php echo $company->name; ?></li>
+                                            </ol>
+                                        <?php
+                                        }
+                                        ?>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="card mb-4">
+                                        <div class="card-body text-center">
+                                            <p>Company</p>
+                                            <img src="<?php echo $companyUser->profilePicture; ?>" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+                                            <h5 class="my-3"><?php echo $company->name; ?></h5>
+                                        </div>
+                                    </div>
+                                    <div class="card mb-4">
+                                        <div class="card-body text-center">
+                                            <p>Driver</p>
+                                            <img src="<?php echo $driverUser->profilePicture; ?>" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+                                            <h5 class="my-3"><?php echo $driver->firstName . " " . $driver->lastName; ?></h5>
+                                            <p class="text-muted mb-1"><?php echo $driver->nationalID; ?></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8">
+                                    <div class="card mb-4">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Vehicle Registration Number</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0"><?php echo $vehicle->registrationNo; ?></p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Vehicle Make</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0"><?php echo $vehicle->make; ?></p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Order Date</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0"><?php echo $order->orderDate; ?></p>
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <p class="mb-0">Status</p>
+                                                </div>
+                                                <div class="col-sm-9">
+                                                    <p class="text-muted mb-0"><?php echo ucfirst($order->status); ?></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="card shadow border-start-primary py-2">
+                                                <div class="card-body">
+                                                    <div class="row align-items-center no-gutters">
+                                                        <div class="col me-2">
+                                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Quantity</span></div>
+                                                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo $order->quantity . "L"; ?></span></div>
+                                                        </div>
+                                                        <div class="col-auto"><i class="fas fa-truck fa-2x text-gray-300"></i></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <div class="card shadow border-start-primary py-2">
+                                                <div class="card-body">
+                                                    <div class="row align-items-center no-gutters">
+                                                        <div class="col me-2">
+                                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Cost</span></div>
+                                                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo "K" . number_format($order->cost); ?></span></div>
+                                                        </div>
+                                                        <div class="col-auto"><i class="fas fa-money-bill fa-2x text-gray-300"></i></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
             <footer class="bg-white sticky-footer">

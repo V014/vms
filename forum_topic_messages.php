7d463ca32c9e7ae0106253d9bf9444bd9488204b
ngomile <ngomile932@gmail.com>
2022-11-28 02:35:51 +0200

    Display messages list for forum topic

--- forum_topic_messages.php
+++ forum_topic_messages.php
@@ -24,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 
 $topic = Topic::find($_GET["id"]);
+$messages = Message::all($topic->id);
 
 ?>
 
@@ -54,37 +55,19 @@ $topic = Topic::find($_GET["id"]);
                     </div>
                 </nav>
                 <div class="container-fluid">
-                    <h3 class="text-dark mb-4">Forum</h3>
-                    <p>Join, View and Create Helpful Forum Discussions</p>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p>Create Forum Topic</p>
-                                    <form class="user" name="forum_topic" method="POST" action="<?php echo htmlentities($_SERVER["PHP_SELF"]); ?>">
-                                        <div class="mb-3">
-                                            <input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="Topic Title" name="title" required>
-                                        </div>
-                                        <button class="btn btn-primary d-block btn-user w-100" type="submit">Submit Topic</button>
-                                        <hr>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="card shadow">
                         <div class="card-header py-3">
-                            <p class="text-primary m-0 fw-bold">Forum Topics and Discussions</p>
+                            <p class="text-primary m-0 fw-bold">Viewing Discussion Messages</p>
                         </div>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12 text-nowrap">
-                                    <span><?php echo count($topics); ?> Topics</span>
+                                    <span><?php echo count($messages); ?> Messages</span>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-6 align-self-center">
-                                    <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of Forum Posts</p>
+                                    <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of Message Posts</p>
                                 </div>
                                 <div class="col-md-6">
                                     <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
@@ -99,75 +82,60 @@ $topic = Topic::find($_GET["id"]);
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-12">
-                                    <?php foreach ($topics as $topic) { ?>
+                                <div class="col">
+                                    <div class="container-fluid mt-100">
                                         <?php
-                                        $messages = Message::all($topic->id);
-                                        $messageCount = count($messages);
-                                        $lastMessage = end($messages);
-                                        $poster = false;
-
-                                        if ($lastMessage) {
-                                            $poster = User::find($lastMessage->userID);
-                                        }
-
-                                        $user = User::find($topic->userID);
+                                        foreach ($messages as $message) {
+                                            $user = user::find($message->userID);
                                         ?>
-                                        <div class="row">
-                                            <div class="container-fluid mt-100">
-                                                <div class="card mb-3">
-                                                    <div class="card-header pl-0 pr-0">
-                                                        <div class="row no-gutters w-100 align-items-center">
-                                                            <div class="col ml-3"></div>
-                                                            <div class="col-4 text-muted">
-                                                                <div class="row no-gutters align-items-center">
-                                                                    <div class="col-4">Replies</div>
-                                                                    <div class="col-8">Last update</div>
-                                                                </div>
-                                                            </div>
+                                            <div class="card mb-3">
+                                                <div class="card-header pl-0 pr-0">
+                                                    <div class="row no-gutters w-100 align-items-center">
+                                                        <div class="col ml-3">
+                                                            <div>Message</div>
+                                                        </div>
+                                                        <div class="col-2 text-muted">
+                                                            <div>Posted By</div>
                                                         </div>
                                                     </div>
-                                                    <div class="card-body py-3">
-                                                        <div class="row no-gutters align-items-center">
-                                                            <div class="col"> <a href="forum_topic_messages.php?id=<?php echo $topic->id; ?>" class="text-big" data-abc="true"><?php echo $topic->title; ?></a>
-                                                                <div class="text-muted small mt-1">Created <?php echo $topic->dateCreated; ?> &nbsp;·&nbsp; <span href="javascript:void(0)" class="text-muted" data-abc="true"><?php echo ucfirst($user->username); ?></span></div>
-                                                            </div>
-                                                            <div class="d-none d-md-block col-4">
-                                                                <div class="row no-gutters align-items-center">
-                                                                    <div class="col-4"><?php echo $messageCount; ?></div>
-                                                                    <div class="media col-8 align-items-center">
-                                                                        <?php if ($poster) { ?>
-                                                                            <img src="<?php echo $poster->profilePicture; ?>" alt="" class="d-block ui-w-30 rounded-circle" style="width: 65px;">
-                                                                            <div class="media-body flex-truncate ml-2">
-                                                                                <div class="line-height-1 text-truncate"><?php echo $lastMessage->dateCreated; ?></div> <a href="javascript:void(0)" class="text-muted small text-truncate" data-abc="true">by <?php echo ucfirst($poster->username); ?></a>
-                                                                            </div>
-                                                                        <?php } else { ?>
-                                                                            <p>No Poster</p>
-                                                                        <?php } ?>
+                                                </div>
+                                                <div class="card-body py-3">
+                                                    <div class="row no-gutters align-items-left">
+                                                        <div class="col">
+                                                            <?php echo $message->body; ?></a>
+                                                            <div class="text-muted small mt-1">Created <?php echo $message->dateCreated; ?> &nbsp;·&nbsp; <span href="javascript:void(0)" class="text-muted" data-abc="true"><?php echo ucfirst($user->username); ?></span></div>
+                                                        </div>
+                                                        <div class="d-none d-md-block col-2">
+                                                            <div class="row no-gutters align-items-center">
+                                                                <div class="media col align-items-center">
+                                                                    <img src="<?php echo $user->profilePicture; ?>" alt="" class="d-block ui-w-30 rounded-circle" style="width: 65px;">
+                                                                    <div class="media-body flex-truncate ml-2">
+                                                                        <div class="line-height-1 text-truncate"><?php echo $message->dateCreated; ?></div> <a href="javascript:void(0)" class="text-muted small text-truncate" data-abc="true">by <?php echo ucfirst($user->username); ?></a>
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <hr>
-                                            <?php } ?>
                                             </div>
-                                        </div>
+                                        <?php } ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <footer class="bg-white sticky-footer">
-                        <div class="container my-auto">
-                            <div class="text-center my-auto copyright"><span>Copyright © Brand 2022</span></div>
-                        </div>
-                    </footer>
-                </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
-            </div>
-            <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-            <script src="assets/js/bs-init.js"></script>
-            <script src="assets/js/theme.js"></script>
+                </div>
+                <footer class="bg-white sticky-footer">
+                    <div class="container my-auto">
+                        <div class="text-center my-auto copyright"><span>Copyright © Brand 2022</span></div>
+                    </div>
+                </footer>
+            </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/theme.js"></script>
 </body>
 
 </html>
